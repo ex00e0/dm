86ebffdad20a6c3eae8980ad09be80c864e56cad
ex00e0 <148848809+ex00e0@users.noreply.github.com>
2024-09-13 21:36:01 +0300

    Add files via upload

---
+++ todo/js/script.js
@@ -0,0 +1,25 @@
+document.getElementById("plus").addEventListener("click", function () {
+    document.getElementById("shadow").style.display = "block";
+    document.getElementById("modal").style.display = "grid";
+});
+document.getElementById("cancel").addEventListener("click", function () {
+    document.getElementById("shadow").style.display = "none";
+    document.getElementById("modal").style.display = "none";
+});
+
+$("#modal").submit(function (e) { // Устанавливаем событие отправки для формы с id=form
+    e.preventDefault();
+     var form_data = $(this).serialize(); // Собираем все данные из формы
+     $.ajax({
+         type: "POST", // Метод отправки
+         url: "../user/add.php", // Путь до php файла отправителя
+         data: form_data,
+         success: function () {
+             
+             document.getElementById("shadow").style.display = "none";
+             document.getElementById("modal").style.display = "none";
+             $('#modal'). trigger('reset');
+            //  alert("Задача добавлена!");
+         }
+     });
+ });
