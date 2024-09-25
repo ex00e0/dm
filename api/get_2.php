f559c4d59b8cc927df16044d3cfa84d2694e7863
ex00e0 <148848809+ex00e0@users.noreply.github.com>
2024-09-25 09:04:36 +0300

    api

---
+++ api/get_2.php
@@ -0,0 +1,16 @@
+<?php
+// Ваш API-ключ
+$apiKey = "15800c3022510d25a95fc0112c100499";
+
+// ID города Уфа (у OpenWeatherMap у каждого города есть свой ID)
+$cityId = "524901"; // ID для Уфы
+
+// Формируем URL для запроса к API OpenWeatherMap
+$url = "http://api.openweathermap.org/data/2.5/weather?id={$cityId}&units=metric&lang=ru&appid={$apiKey}";
+
+// Выполняем запрос к API
+$response = file_get_contents($url);
+
+// Выводим полученные данные
+echo $response;
+?>
